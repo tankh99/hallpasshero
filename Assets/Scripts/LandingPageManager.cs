d73ed9644ed0f4a1408d4f9d7fa1a2c369637073
goodwilllion <goodwilllion28@gmail.com>
2025-03-29 23:36:05 -0500

    feat: exit button landingscene

--- Assets/Scripts/LandingPageManager.cs
+++ Assets/Scripts/LandingPageManager.cs
@@ -6,12 +6,15 @@ using UnityEngine.SceneManagement;
 public class LandingPageManager : MonoBehaviour
 {
     public Button startGameButton;
+    public Button exitGameButton;
     
     private void Start()
     {
         // Set up button listeners
         if (startGameButton != null)
             startGameButton.onClick.AddListener(StartGame);
+        if (exitGameButton != null)
+            exitGameButton.onClick.AddListener(ExitGame);
     }
     
     private void StartGame()
@@ -21,12 +24,12 @@ public class LandingPageManager : MonoBehaviour
     }
     
     
-    // private void QuitGame()
-    // {
-    //     #if UNITY_EDITOR
-    //         UnityEditor.EditorApplication.isPlaying = false;
-    //     #else
-    //         Application.Quit();
-    //     #endif
-    // }
+    private void ExitGame()
+    {
+        #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+        #else
+            Application.Quit();
+        #endif
+    }
 } 
