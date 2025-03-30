9dea65a240c222f9db987f9d4dfed7dc2e9b3247
goodwilllion <goodwilllion28@gmail.com>
2025-03-29 22:53:50 -0500

    feat: connect to MainGame scene

--- Assets/Scripts/LandingPageManager.cs
+++ Assets/Scripts/LandingPageManager.cs
@@ -5,43 +5,13 @@ using UnityEngine.SceneManagement;
 
 public class LandingPageManager : MonoBehaviour
 {
-    [Header("UI Elements")]
-    [SerializeField] private Button startGameButton;
-    [SerializeField] private Button settingsButton;
-    [SerializeField] private Button quitButton;
-    [SerializeField] private TextMeshProUGUI titleText;
-    
-    [Header("Animation")]
-    [SerializeField] private float titlePulseSpeed = 1f;
-    [SerializeField] private float titlePulseScale = 1.1f;
+    public Button startGameButton;
     
     private void Start()
     {
         // Set up button listeners
         if (startGameButton != null)
             startGameButton.onClick.AddListener(StartGame);
-        if (settingsButton != null)
-            settingsButton.onClick.AddListener(OpenSettings);
-        if (quitButton != null)
-            quitButton.onClick.AddListener(QuitGame);
-            
-        // Start title animation
-        if (titleText != null)
-            StartCoroutine(PulseTitle());
-    }
-    
-    private System.Collections.IEnumerator PulseTitle()
-    {
-        Vector3 originalScale = titleText.transform.localScale;
-        float time = 0;
-        
-        while (true)
-        {
-            time += Time.deltaTime * titlePulseSpeed;
-            float scale = Mathf.Lerp(1f, titlePulseScale, Mathf.PingPong(time, 1f));
-            titleText.transform.localScale = originalScale * scale;
-            yield return null;
-        }
     }
     
     private void StartGame()
@@ -50,18 +20,13 @@ public class LandingPageManager : MonoBehaviour
         SceneManager.LoadScene("MainGame");
     }
     
-    private void OpenSettings()
-    {
-        // TODO: Implement settings menu
-        Debug.Log("Settings menu not implemented yet");
-    }
     
-    private void QuitGame()
-    {
-        #if UNITY_EDITOR
-            UnityEditor.EditorApplication.isPlaying = false;
-        #else
-            Application.Quit();
-        #endif
-    }
+    // private void QuitGame()
+    // {
+    //     #if UNITY_EDITOR
+    //         UnityEditor.EditorApplication.isPlaying = false;
+    //     #else
+    //         Application.Quit();
+    //     #endif
+    // }
 } 
